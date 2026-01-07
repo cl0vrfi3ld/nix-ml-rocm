2b75b848bee8860bd7388d17e319adcda958082f
cl0vrfi3ld <cl0vr@noreply.git.cl0vr.co>
2026-01-06 22:21:01 -0500

    add torchaudio and torchvision build support to script

--- build_cache.py
+++ build_cache.py
@@ -12,7 +12,7 @@ CACHE_NAME: str = "ml-rocm"
 SUPPORTED_PYTHON_VERSIONS = Literal["311", "312"]  # TODO: add 3.13 support
 PYTHON_VERSIONS: list[SUPPORTED_PYTHON_VERSIONS] = [
     "311", "312"]  # TODO: add 3.13 support
-PYTHON_PACKAGES: list[str] = ["torch"]  # TODO: add torchaudio and torchvision
+PYTHON_PACKAGES: list[str] = ["torch", "torchaudio", "torchvision"]
 
 
 def log(level: str, msg: str, **kwargs: Any) -> None:
